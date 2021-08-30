ac3d45b10e6fc964d6828a5bf521a49038d9381e
Chuks Akibot <juniorakibor@yahoo.com>
2021-08-30 17:25:43 +0200

    cookies primer functionality added

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
 const app = express();
 dotenv.config();
+
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 // database connection
-const dbURI =
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@testcluster1.6edzl.mongodb.net/node-auth`;
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@testcluster1.6edzl.mongodb.net/node-auth`;
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
@@ -33,3 +35,25 @@ mongoose
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  const one_day = 1000 * 60 * 60 * 24;
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, {
+    maxAge: one_day,
+    httpOnly: true,
+    secure: true,
+  });
+
+  res.send('you got the cookies');
+});
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+});
